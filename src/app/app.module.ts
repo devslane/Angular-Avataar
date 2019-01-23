d40614448240b1005ea5a08e8b83edbfed72b187
shivam garg <gargshivam482@gmail.com>
2019-01-23 20:40:49 +0530

    ui model created for form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AvtaarComponent } from './avtaar/avtaar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatRadioModule} from '@angular/material/radio'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -11,7 +18,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatRadioModule,
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
